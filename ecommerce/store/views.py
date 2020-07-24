a223f76d6bc6dc38ee81ea348445eafe6fc6bfa6
yo14 <ifliandry11@gmail.com>
2020-07-25 04:58:37 +0700

    Materi: Membuat Cart dan Checkout Page Menjadi Dinamis

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -11,7 +11,7 @@ def store(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_item':0}
+        order = {'get_cart_total':0, 'get_cart_item':0, 'shipping':False}
         cartItems = order['get_cart_items']
 
     products = Product.objects.all()
@@ -30,7 +30,7 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_item':0}
+        order = {'get_cart_total':0, 'get_cart_item':0, 'shipping':False}
         cartItems = order['get_cart_items']
 
     context = {
@@ -48,7 +48,7 @@ def checkout(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_item':0}
+        order = {'get_cart_total':0, 'get_cart_item':0, 'shipping':False}
         cartItems = order['get_cart_items']
 
     context = {
