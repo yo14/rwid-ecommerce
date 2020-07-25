202bf9b5ddcb92df9086672834af8f090be78d06
yo14 <ifliandry11@gmail.com>
2020-07-26 00:46:18 +0700

    Materi: Penyelesaian Checkout Page

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.http import JsonResponse
 from django.shortcuts import render
 from .models import *
@@ -84,5 +86,32 @@ def updateItem(request):
     return JsonResponse('Item was added', safe=False)
 
 def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+
+    else:
+        print('User is not logged in')
+
     return JsonResponse('Payment complete!', safe=False)
 
